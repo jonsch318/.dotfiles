043e4000439727709c4ad19c609292c90ed9eb3a
Gobidev <adrian.groh@t-online.de>
2022-10-15 18:18:18 +0200

    Fix nvim lsp deprecation warnings

--- .config/nvim/lua/gobidev/lsp/handlers.lua
+++ .config/nvim/lua/gobidev/lsp/handlers.lua
@@ -49,7 +49,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.documentHighlightProvider then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -122,6 +122,6 @@ if not status_ok then
 	return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
