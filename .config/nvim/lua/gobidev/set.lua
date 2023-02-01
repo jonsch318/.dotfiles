4ca5aacb0fa129ec3f0f873d5ab9474f528696a6
Adrian Groh <adrian.groh@t-online.de>
2023-02-01 14:46:20 +0100

    nvim: autocmds for file specific options

--- .config/nvim/lua/gobidev/set.lua
+++ .config/nvim/lua/gobidev/set.lua
@@ -14,11 +14,22 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.cmd([[
+    autocmd FileType markdown setlocal ts=2 sts=2 sw=2
+    autocmd FileType json setlocal ts=2 sts=2 sw=2
+    autocmd FileType jsonc setlocal ts=2 sts=2 sw=2
+    autocmd FileType json5 setlocal ts=2 sts=2 sw=2
+    autocmd FileType toml setlocal ts=2 sts=2 sw=2
+    autocmd FileType make setlocal ts=4 sts=0 sw=4 noexpandtab
+]])
 
 vim.opt.smartindent = true
 
 -- wrapping
 vim.opt.wrap = false
+vim.cmd([[
+    autocmd FileType markdown setlocal wrap
+]])
 vim.opt.showbreak = "↪"
 
 vim.opt.scrolloff = 8
@@ -37,12 +48,19 @@ vim.opt.termguicolors = true
 
 -- column at 120 characters to indicate max line width
 vim.opt.colorcolumn = "120"
+vim.cmd([[
+     autocmd FileType gitcommit setlocal colorcolumn=72
+    autocmd FileType python setlocal colorcolumn=79
+    autocmd FileType markdown setlocal colorcolumn=80
+    autocmd FileType rust setlocal colorcolumn=100
+]])
 
 -- disable mode display because it is handled by airline
 vim.opt.showmode = false
 
--- VIMSCRIPT SECTION
-vim.cmd [[
+vim.g.vim_markdown_folding_disabled = 1
+
+vim.cmd([[
     " Highlight yanking
     au TextYankPost * silent! lua vim.highlight.on_yank()
 
@@ -51,4 +69,4 @@ vim.cmd [[
      \ if line("'\"") > 0 && line("'\"") <= line("$") |
      \   exe "normal! g`\"" |
      \ endif
-]]
+]])
