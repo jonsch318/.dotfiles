ec2852c5ac5c4466f3a1db56cce2389439c971e0
Adrian Groh <adrian.groh@t-online.de>
2023-06-22 17:29:45 +0200

    nvim: add typst plugin

--- .config/nvim/lua/gobidev/packer.lua
+++ .config/nvim/lua/gobidev/packer.lua
@@ -93,6 +93,7 @@ return packer.startup(function(use)
     use('williamboman/mason.nvim')
     use('williamboman/mason-lspconfig.nvim')
     use('jayp0521/mason-null-ls.nvim')
+    use {'kaarmu/typst.vim', ft = {'typst'}}
     use('RubixDev/mason-update-all')
     use {
         'jose-elias-alvarez/null-ls.nvim',
