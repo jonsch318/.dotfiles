d6ccfde7a32a150c123cc6c51e3be6ba0a5f07b5
Jonas Schneider <jonas.max.schneider@gmail.com>
2025-06-03 13:26:48 +0200

    Move to new treesitter

--- dotconfig/nvim/after/ftplugin/markdown.lua
+++ dotconfig/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 vim.opt.spelllang = "en_us,de_de"
 vim.opt.spell = true
+vim.treesitter.start()
--- dotconfig/nvim/lua/plugins/treesitter.lua
+++ dotconfig/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,39 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		build = ":TSUpdate",
-		opts = {
-			highlight = {
-				enable = true,
-			},
-			{
-				context_commentstring = {
-					enable = true,
-					enable_autocmd = false,
-				},
-			},
-			textobjects = {
-				move = {
-					enable = true,
-					goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-					goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-					goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-				},
-			},
-			endwise = {
-				enable = true,
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-			require("config.filetypes").treesitter_setup()
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            -- "nvim-treesitter/nvim-treesitter-textobjects",
+        },
+        lazy = false,
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+        branch = "main",
+        build = ":TSUpdate",
+        opts = {
+            -- highlight = {
+            --     enable = true,
+            -- },
+            -- {
+            --     context_commentstring = {
+            --         enable = true,
+            --         enable_autocmd = false,
+            --     },
+            -- },
+            -- textobjects = {
+            --     move = {
+            --         enable = true,
+            --         goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+            --         goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+            --         goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+            --         goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+            --     },
+            -- },
+            -- endwise = {
+            --     enable = true,
+            -- },
+        },
+        -- config = function(_, opts)
+        --     require("nvim-treesitter.configs").setup(opts)
+        --     require("config.filetypes").treesitter_setup()
+        -- end,
+    },
 }
